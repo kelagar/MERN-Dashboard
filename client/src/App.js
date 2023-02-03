1fe8859d6915c504097c946dda5e932a437872f6
Aditya Kelagar <aditya.kelagar@redtraitventures.com>
2023-02-03 13:45:54 -0500

    setup mock data

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div ClassName="app"></div>;
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
 import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
 import mangementRoutes from "./routes/management.js";
@@ -15,10 +15,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 //ROUTES
@@ -29,11 +29,14 @@ app.use("/sales", salesRoutes);
 
 //MONGOOSE SETUP
 const PORT = process.env.PORT || 9000;
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewURLParser: true,
-    useUnifiedTopology: true, 
-}).then(() => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-}).catch((error) => {
-    console.log('Error connecting: ',  error);
-})
+  })
+  .catch((error) => {
+    console.log("Error connecting: ", error);
+  });
